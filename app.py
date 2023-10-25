286841a7cb3aa0d483456b7beb27ac4854aa4fe5
Anthony Wang <anthonywang64@gmail.com>
2023-10-25 14:58:53 -0400

    simple login/register pages with functionality; no personalized collections for users yet

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request, redirect
+from flask import Flask, render_template, Response, request, redirect, session, current_app
 import cv2
 import datetime
 import os
@@ -17,7 +17,10 @@ except OSError as error:
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test2.db'
+# Add a secret key for session management
+app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
+
 
 class Collection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,6 +33,23 @@ class Collection(db.Model):
         return '<Task %r>' % self.id
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+    def check_password(self, password):
+        return self.password == password
+
+
+# Create the database tables within the application context
+with app.app_context():
+    db.create_all()
+
+
 def generate_frames():
     global capture
     while True:
@@ -38,43 +58,109 @@ def generate_frames():
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+            ret, buffer = cv2.imencode('.jpg', cv2.flip(frame, 1))
             frame = buffer.tobytes()
             if not ret:
                 continue
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        if(capture):
-            capture=0
+        if (capture):
+            capture = 0
             now = datetime.datetime.now()
-            p = os.path.sep.join(['photos', "photo_{}.png".format(str(now).replace(":",''))])
+            p = os.path.sep.join(
+                ['photos', "photo_{}.png".format(str(now).replace(":", ''))])
             cv2.imwrite(p, frame)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    logged_in = 'username' in session
+    return render_template('index.html', logged_in=logged_in, current_user=session.get('username'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        # Assuming you have a check_password method in your User model
+        if user and user.check_password(password):
+            session['username'] = user.username
+            return redirect('/')
+        else:
+            return render_template('login.html', login_failed=True)
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        new_username = request.form['username']
+        new_password = request.form['password']
+        new_confirm_password = request.form['confirm_password']
+        new_user = User(username=new_username, password=new_password)
+
+        # Check if the username is already taken
+        existing_user = User.query.filter_by(username=new_username).first()
+        if existing_user:
+            return render_template('register.html', registration_failed=True)
+
+        # Check if the password and confirm_password match
+        if new_password != new_confirm_password:
+            return render_template('register.html', registration_failed=True)
+
+        # Create a new user
+        new_user = User(username=new_username, password=new_password)
+
+        try:
+            # Add the new user to the database
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = new_username
+
+            # Redirect to the main page after registration
+            return redirect('/')
+        except Exception as e:
+            print(str(e))
+            return 'There was an issue with registration. Please try again.'
+
+    return render_template('register.html')
+
 
 @app.route('/video')
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/take_photo', methods=['POST','GET'])
+
+@app.route('/take_photo', methods=['POST', 'GET'])
 def take_photo():
     return render_template('take_photo.html', title='Take Photo')
 
-@app.route('/tasks', methods=['POST','GET'])
+
+@app.route('/tasks', methods=['POST', 'GET'])
 def tasks():
     if request.method == 'POST':
-            if request.form.get('click') == 'Capture':
-                global capture
-                capture=1
-                return "photo captured"
-            else:
-                return "fail"
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture = 1
+            return "photo captured"
+        else:
+            return "fail"
+
 
 @app.route('/upload_photo')
 def upload_photo():
     return render_template('upload_photo.html', title='Upload Photo')
+
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
@@ -87,7 +173,8 @@ def upload():
         # for now, the file isjust going to this directory, but we will need to connect this to our db/image storage system
         file.save("uploads/" + file.filename)
         return 'File uploaded successfully'
-    
+
+
 @app.route('/collection', methods=['POST', 'GET'])
 def collection():
     if request.method == 'POST':
@@ -105,6 +192,7 @@ def collection():
         tasks = Collection.query.order_by(Collection.date_created).all()
         return render_template('collection.html', title='View Collection', tasks=tasks)
 
+
 @app.route('/delete/<int:id>')
 def delete(id):
     furto_to_delete = Collection.query.get_or_404(id)
@@ -115,7 +203,8 @@ def delete(id):
         return redirect('/collection')
     except:
         return 'There was a problem deleting that task'
-    
+
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     furto = Collection.query.get_or_404(id)
@@ -131,6 +220,7 @@ def update(id):
             return 'There was an issue updating your furto, so sorry!'
     else:
         return render_template('update.html', task=furto)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
