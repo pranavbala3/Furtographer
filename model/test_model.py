f082a249230704b6c94e913bc0f882be46e920ac
Timothy Cronin <tcroniniv@gmail.com>
2023-11-13 21:34:03 -0500

    test ingestion

---
+++ model/test_model.py
@@ -0,0 +1,20 @@
+from .model import Model
+import os
+
+model = Model()
+current_dir = os.path.dirname(os.path.abspath(__file__))
+images_path = os.path.join(current_dir, 'images')
+
+
+# ensure the model ingests images of all sizes
+def ingesting_different_sizes():
+    for filename in os.listdir(images_path):
+        if filename != '.DS_Store':
+            full_path = os.path.join(images_path, filename)
+            model.predict_path(full_path)
+            img = model.form_image(full_path)
+            model.is_dog(img)
+            model.predict_breed(img)
+
+
+ingesting_different_sizes()
