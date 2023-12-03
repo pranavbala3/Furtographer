db3d91a4ca40865daaaaf9fa62fc1742d5467498
Anthony Wang <anthonywang64@gmail.com>
2023-12-03 01:45:15 -0500

    added functionality to personalized collections table

--- database/sql/tables/V20231025_0909__create_table_collections.sql
+++ database/sql/tables/V20231025_0909__create_table_collections.sql
@@ -1,9 +1,10 @@
-create table collections (
-    id   bigserial not null,
-    content  varchar(200) not null,
-    breed  varchar(200) not null,
-    completed  integer default 0,
-    date_created  timestamp default current_timestamp,
-    
-    primary key (id)
+CREATE TABLE collections (
+    id           BIGSERIAL       NOT NULL,
+    content      VARCHAR(200)    NOT NULL,
+    breed        VARCHAR(200)    NOT NULL,
+    completed    INTEGER         DEFAULT 0,
+    date_created TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    user_id      INTEGER         REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    PRIMARY KEY (id)
 );
