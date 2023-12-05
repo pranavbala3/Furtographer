1b4ecfd129c38a8dab5b5fddabb1abe14389504a
Anthony Wang <anthonywang64@gmail.com>
2023-12-05 00:56:44 -0500

    take_photo and upload photo functionality and adds it to database

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def generate_frames():
     global retake
     global latest_frame
     global latest_breedname
+    global photo_path
 
     camera = cv2.VideoCapture(0)
     while camera.isOpened():
@@ -123,8 +124,6 @@ def generate_frames():
                 breedname = str(breed).replace('_', ' ')
                 latest_breedname = breedname
                 photo_path = p
-                print(photo_path)
-                Collection.add(p, latest_breedname, session.get('user_id'))
                 save = 0
             elif retake:
                 retake = 0
@@ -223,30 +222,39 @@ def video():
 
 @app.route('/take_photo', methods=['POST', 'GET'])
 def take_photo():
+    global photo_path
+    global latest_breedname
     logged_in = 'username' in session
+    
+    if photo_path and latest_breedname:
+        Collection.add(photo_path, latest_breedname, session.get('user_id'))
+        photo_path = None
+        latest_breedname = None
+        
     return render_template('take_photo.html', logged_in=logged_in, current_user=session.get('username'), title='Take Photo', breedname = latest_breedname)
 
 
 @app.route('/tasks', methods=['POST', 'GET'])
 def tasks():
+    logged_in = 'username' in session
     if request.method == 'POST':
         if request.form.get('click') == 'Capture':
             global capture
             capture = 1
             generate_frames()
             # Trigger the display message
-            return render_template('take_photo.html', title='Take Photo' , show_modal=True, upload=False, breed=latest_breedname)
+            return render_template('take_photo.html', logged_in=logged_in, title='Take Photo' , show_modal=True, upload=False, breed=latest_breedname)
         elif request.form.get('click') == 'Save':
             global save
             save = 1
-            return render_template('take_photo.html', title='Take Photo', show_modal=False, upload=True, breed=latest_breedname, uploaded_image_url=photo_path)
+            return render_template('take_photo.html', logged_in=logged_in,  title='Take Photo', show_modal=False, upload=True, breed=latest_breedname, uploaded_image_url=photo_path)
         elif request.form.get('click') == 'Retake':
             global retake
             retake = 1
-            return render_template('take_photo.html', title='Take Photo', show_modal=False, upload=False, breed=latest_breedname)
+            return render_template('take_photo.html', logged_in=logged_in, title='Take Photo', show_modal=False, upload=False, breed=latest_breedname)
         else:
             return "fail"
-    return render_template('take_photo.html', show_modal=False, upload=False, breed=latest_breedname)
+    return render_template('take_photo.html', logged_in=logged_in, show_modal=False, upload=False, breed=latest_breedname)
 
 
 @app.route('/upload_photo')
@@ -274,6 +282,9 @@ def upload():
             breedname = "picture without dawgs :("
         else:
             breedname = str(breed).replace('_', ' ')
+
+        # Save file information to the database
+        Collection.add(file_path, breedname, session.get('user_id'))    
         return render_template('upload_photo.html', title='Upload Photo', upload_error=False, upload=True, breed=breedname, \
             uploaded_image_url=file_path)
 
